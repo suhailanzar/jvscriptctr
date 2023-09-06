de494f1fb4205b0ed72846abf4b045d6264d4a18
suhail ansar <125738472+suhailanzar@users.noreply.github.com>
2023-09-06 11:37:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let countEl= document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+
+
+let count=0
+
+function increment(){
+
+
+count +=1
+
+countEl.textContent=count
+
+console.log(count)
+
+}
+
+function save(){
+  let countstr=count + "-"
+    saveEl.textContent+= countstr
+    countEl.textContent=0
+    count=0
+
+}
+
+
+
